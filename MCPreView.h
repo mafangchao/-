8f765cef266a6e5789f540ce5f96fff5130f6a7a
自定义提示框 <ios@ios001deMacBook-Pro.local>
2016-06-30 09:38:49 +0800

    二维码扫描中的view

---
+++ MCPreView.h
@@ -0,0 +1,14 @@
+//
+//  MCPreView.h
+//  myApp
+//
+//  Created by iOS on 16/6/15.
+//  Copyright © 2016年 careerman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <AVFoundation/AVFoundation.h>
+
+@interface MCPreView : UIView
+@property(nonatomic,strong)AVCaptureSession *session;
+@end
